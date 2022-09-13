6ca2229d4c4edc8ca50953062e73b3463b01f98e
kimjos <josephkimani500@gmail.com>
2022-09-13 11:59:24 -0700

    printing alphabetx10

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,5 +14,12 @@ int _putchar(char c);
  *
  */
 void print_alphabet(void);
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: always 0;
+ *
+ */
+void print_alphabet_x10(void);
 #endif
 
