76de97648d58deeba085a9196216efa08c49fab0
kimjos <josephkimani500@gmail.com>
2022-09-26 09:56:51 -0700

    search needle in haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - finds needle in haystack
+ *
+ * @needle: substring to search
+ * @haystack: string to be searched
+ * Return: needle
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+
+	}
+	return ('\0');
+}
