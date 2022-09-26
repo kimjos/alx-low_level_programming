6d07ad7578d00004c0e8bd0fc7419f16966c87ac
kimjos <josephkimani500@gmail.com>
2022-09-26 07:40:24 -0700

    memset function

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is readme for pointers, arrays and string.
+Even more pointers, arrays and strings.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
