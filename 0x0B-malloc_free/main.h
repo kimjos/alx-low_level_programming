5d3ade82ddcbc85b4916511c72375ad4fe4c7b28
kimjos <josephkimani500@gmail.com>
2022-10-04 10:12:45 -0700

    concatenate two strings

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,14 @@
 #ifndef _PUTCHAR_H
 #define _PUTCHAR_H
 
+#include <stdio.h>
+
 char *create_array(unsigned int size, char c);
 
 int _putchar(char c);
 
 char *_strdup(char *str);
+
+char *str_concat(char *s1, char *s2);
 
 #endif
