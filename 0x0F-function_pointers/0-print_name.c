45ce398991f32f632069ebfda899e3ff00d87168
kimjos <josephkimani500@gmail.com>
2022-10-13 11:37:17 -0700

    printa name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: the name to be printed
+ * @f: the function to do the printing
+ *
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+
+int _putchar(char c);
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
