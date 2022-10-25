76756753874d31cb70bad597d7350c72ee8aab0c
kimjos <josephkimani500@gmail.com>
2022-10-25 07:11:54 -0700

    print all

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: the list_t list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int numnodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		numnodes++;
+	}
+	return (numnodes);
+}
