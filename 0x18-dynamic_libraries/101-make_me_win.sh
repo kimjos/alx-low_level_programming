5859364a41cc0de158a2b9a5e26f9f00d7e21779
kimjos <josephkimani500@gmail.com>
2024-01-31 23:59:46 -0800

    not there

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Pericles001/alx-low_level_programming/master/0x18-dynamic_libraries/hack.so
+wget -P /tmp/ https://github.com/kimjos/alx-low_level_programming/blob/main/0x18-dynamic_libraries/hack.so
 export LD_PRELOAD =/tmp/hack.o
